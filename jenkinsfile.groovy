4ba11fd883d69be712bd3dcec4ea8345cfccbaf3
Ramji-Chanamala <51015242+Ramji-Chanamala@users.noreply.github.com>
2021-01-23 10:54:40 +0530

    Create jenkinsfile.groovy

---
+++ jenkinsfile.groovy
@@ -0,0 +1,34 @@
+pipeline {
+
+    agent any
+    tools {
+        // Install the Maven version configured as "M3" and add it to the path.
+        maven "onlinemaven"
+    }
+    stages{
+        stage('checkout') {
+            steps{
+                //checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[url: 'https://github.com/Ramji-Chanamala/DevOpsClassCodes.git']]])
+                git 'https://github.com/Ramji-Chanamala/DevOpsClassCodes.git'
+            }
+        }
+        stage('compile'){
+            steps {
+                sh 'mvn compile'
+            } 
+        }
+        stage('test') {
+            steps {
+                sh 'mvn test'
+            }
+        }
+        stage('cobertura'){
+            steps {
+                sh 'mvn clean cobertura:cobertura -Dcobertura.report.format=xml'
+            }
+
+        }          
+        
+
+    }    
+}
