8cef19ccf4751c14068b0ef97017a1df16d90f0c
Ramji-Chanamala <51015242+Ramji-Chanamala@users.noreply.github.com>
2021-07-20 21:57:37 +0530

    Update jenkins.groovy

--- jenkins.groovy
+++ jenkins.groovy
@@ -3,8 +3,7 @@ pipeline {
     agent any
     tools {
         // Install the Maven version configured as "M3" and add it to the path.
-        maven 'Maven 3.3.9' 
-        jdk 'jdk8' 
+        maven 'Maven' 
     }
     stages{
         stage('checkout') {
